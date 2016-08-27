54963f773a457dd5275ad9d8864cc6c3c1229c65
Mikołaj Pich <m.pich@outlook.com>
2016-08-27 11:31:10 +0200

    Fix ga script code

--- hooks.php
+++ hooks.php
@@ -2,7 +2,7 @@
 
 use Rudolf\Component\Hooks;
 
-Hooks\Filter::add('foot_after', function ($after) {
+Hooks\Filter::add('head_after', function ($after) {
     $file = CONFIG_ROOT.'/plugins/google-analytics.php';
     if (!file_exists($file)) {
         return $after;die;
@@ -15,7 +15,7 @@ Hooks\Filter::add('foot_after', function ($after) {
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,"script","//www.google-analytics.com/analytics.js","ga");
-      ga("create", '.$config['ga_id'].', "auto");
+      ga("create", "'.$config['ga_id'].'", "auto");
       ga("send", "pageview");
     </script>';
 
